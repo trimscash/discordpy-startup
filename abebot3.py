b9a186e65d63f0f8728199cb635f2cd0eb9c8179
daichi0629 <42578480+daichi0629@users.noreply.github.com>
2020-04-10 22:53:55 +0900

    Add files via upload

---
+++ abebot3.py
@@ -0,0 +1,28 @@
+import discord
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Logged in as")
+    print(client.user.name)
+    print(client.user.id)
+    print("------")
+
+@client.event
+async def on_message(message):
+    if ("よろ" in message.content):
+        if client.user != message.author:
+            await message.channel.send("よろぴくついでに投票よろ")
+
+
+    if ("マスク" in message.content):
+        if client.user != message.author:
+            await message.channel.send("マスク二枚くれてやるよ")
+            
+           
+
+
+
+            
+client.run("Njk4MDgxMTA0MDQwNTU4NjIy.XpA2kQ.sTpPBAosGqAHGnEklBRBr7QjQLQ")
+
