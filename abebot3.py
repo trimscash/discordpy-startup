81800137d9791157fa81b90c4ecf337578df1beb
daichi0629 <42578480+daichi0629@users.noreply.github.com>
2020-05-09 00:25:13 +0900

    Update abebot3.py

--- abebot3.py
+++ abebot3.py
@@ -1,3 +1,4 @@
+import random
 import discord
 from discord.ext import commands
 import os
@@ -23,6 +24,7 @@ async def on_message(message):
         if client.user != message.author:
             my="よろぴくついでに投票よろ"
             await message.channel.send(my)
+            
 
     if ("マスク" in message.content)or("コロナ" in message.content):
         if client.user != message.author:
@@ -39,6 +41,7 @@ async def on_message(message):
        ("安倍"in message.content)and ("やろ"in message.content) :
         if client.user != message.author:
             await message.channel.send("やりたければ国会議事堂にこい")
+            continue
 
     if ("昭恵" in message.content):
         if client.user != message.author:
@@ -56,10 +59,10 @@ async def on_message(message):
         if client.user != message.author:
             await message.channel.send("はい。昭恵～")
 
-    if ("abeskip " in message.content):
-        if client.user != message.author:
-            music6 = message.content.lstrip("abeskip")
-            await message.channel.send("!playskip"+ music6)
+#    if ("abeskip " in message.content):
+ #       if client.user != message.author:
+  #          music6 = message.content.lstrip("abeskip")
+   #         await message.channel.send("!playskip"+ music6)
 
 
     if ("緊急事態宣言　"in message.content):
@@ -73,12 +76,18 @@ async def on_message(message):
            
     if ("!p www" in message.content):
         if client.user != message.author:
-            await message.channel.send("いいね！")
+             if random.randint(0,13) == 0:
+                await message.channel.send("いいね！")
       
     if message.content.startswith("abe"):
         if client.user != message.author:
-            await message.channel.send("https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Shinz%C5%8D_Abe_Official.jpg/240px-Shinz%C5%8D_Abe_Official.jpg")
-            
+            a = random.randint(0,2)
+            if a == 0:
+                await message.channel.send("https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Shinz%C5%8D_Abe_Official.jpg/240px-Shinz%C5%8D_Abe_Official.jpg")
+            elif a == 1:
+                await message.channel.send("https://portal.st-img.jp/detail/47b60bc4550ae36ebf425f6926176ee5_1588586592_2.jpg")
+            elif a == 2:
+                await message.channel.send("https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Akie_Abe.jpg/200px-Akie_Abe.jpg")
 
     
             
